bdb8cb1c6f5c1777abc0dceba748750fba6f3254
semilore317 <semi.banks.317@gmail.com>
2025-01-02 23:57:49 +0100

    more skeletal code...

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 # TODO 1: add a functioning side bar
-# TOOD 2: add a functioning menu bar 
+# TODO 2: add a functioning menu bar
 # TODO 3 : add the main app GUI for creating custom fields for passwords
 # TODO 4: add the file handling logic to store the passwords
 # TODO 5: use cryptography module to store the credentials as hashes
 
-import tkinter as tk
+#import tkinter as tk
 import customtkinter as ctk
 
 # functions
+def close_welcome_label():
+    loading_label.destroy()
+
 def close_welcome_screen():
-    root.destroy()  # Closes the welcome screen
+    welcome_label.destroy()  # Closes the welcome screen
+    main_frame.pack(fill="both", expand=True)
+    root.after(1500, close_welcome_label)
 
 # Basic theming
 ctk.set_appearance_mode("system")
@@ -20,13 +25,10 @@ root.geometry("700x480")
 root.title("Locksmith")
 root.resizable(width=False, height=False)
 
-frame = ctk.CTkFrame(master=root)
-frame.pack(padx=10, pady=10, fill="both", expand=True)
-
 # Add content to the welcome screen
 welcome_label = ctk.CTkLabel(
     root, 
-    text="Welcome to My Application", 
+    text="Welcome to Locksmith!", 
     font=ctk.CTkFont("Arial", size=24, weight="bold")
 )
 welcome_label.pack(pady=100)
@@ -41,6 +43,16 @@ loading_label.pack(pady=10)
 # Schedule the screen to close after 3 seconds (3000 milliseconds)
 root.after(3000, close_welcome_screen)
 
+# main app frame after welcome screen
+main_frame = ctk.CTkFrame(master=root, width=700, height=400)
+
+# sidebar
+side_bar = ctk.CTkFrame(master=root, width=150, height=400)
+
+#menu bar
+menu_bar = ctk.CTkOptionMenu(master=main_frame)
+file_menu = ctk.CTkOptionMenu(master=main_frame)
+action_menu = ctk.CTkOptionMenu(master=main_frame)
 
 root.mainloop()
 
