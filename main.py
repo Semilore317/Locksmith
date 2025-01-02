bef6680f35ac24e262b19257180f3b2606fa5270
semilore317 <semi.banks.317@gmail.com>
2025-01-02 21:27:13 +0100

    removed the zipFIle app biolerplate

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ customtkinter.set_default_color_theme("dark-blue")
 
 root = customtkinter.CTk()
 root.geometry("600x600")
-root.title("ZipWizard!")
+root.title("Locksmith")
 root.resizable(width=False, height=False)
 
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(padx=10, pady=10, fill="both", expand=True)
 
 # Welcome-label
-welcomeLabel = customtkinter.CTkLabel(master=frame, text="Welcome to ZipWizard!", font=("Roboto", 40))
+welcomeLabel = customtkinter.CTkLabel(master=frame, text="Welcome to Locksmith", font=("Roboto", 40))
 welcomeLabel.pack(padx=10, pady=10, fill="both", expand=True)
 
-# Select Operation Label
+'''# Select Operation Label
 selectionLabel = customtkinter.CTkLabel(master=frame, text="Select Operation", font=("Roboto", 20))
 selectionLabel.pack(padx=10, pady=10, fill="both", expand=True)
 
@@ -35,7 +35,7 @@ zipButton = customtkinter.CTkButton(master=innerButtonFrame, text="Zip", font=("
 zipButton.grid(row=0, column=0, padx=10, pady=10)
 
 unzipButton = customtkinter.CTkButton(master=innerButtonFrame, text="Unzip", font=("Roboto", 20))
-unzipButton.grid(row=0, column=1, padx=10, pady=10)
+unzipButton.grid(row=0, column=1, padx=10, pady=10)'''
 
 root.mainloop()
 
