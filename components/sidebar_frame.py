62e0027742f9011a23e6075a469be635927fdde6
Hermes <hermeschigoziri@gmail.com>
2025-01-19 19:20:58 +0100

    add sidebar frame and enhance layout structure

---
+++ components/sidebar_frame.py
@@ -0,0 +1,6 @@
+import customtkinter
+
+
+class SidebarFrame(customtkinter.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from tkinter import PhotoImage
 import customtkinter
 from components.header_frame import HeaderFrame
+from components.sidebar_frame import SidebarFrame
+from PIL import Image
 
 customtkinter.set_appearance_mode("dark")
 # customtkinter.set_default_color_theme("dark-blue")
+
 
 class App(customtkinter.CTk):
     def __init__(self):
@@ -20,12 +23,46 @@ class App(customtkinter.CTk):
         self.wm_iconphoto(True, icon)
 
         # header
-        self.header = HeaderFrame(self)
-        self.header.grid(row=0, column=0, sticky="ew")
+        self.header_frame = HeaderFrame(self, fg_color="#222222")
+        self.header_frame.grid(row=0, column=0, sticky="ew")
 
         # Body
-        self.body = customtkinter.CTkFrame(self)
-        self.body.grid(row=1, column=0, sticky="nsew")
+        self.body_frame = customtkinter.CTkFrame(self, border_width=2)
+        self.body_frame.grid(row=1, column=0, sticky="nsew")
+        self.body_frame.grid_columnconfigure(0, weight=1)
+        self.body_frame.grid_columnconfigure((1, 2), weight=3)
+        self.body_frame.grid_rowconfigure(0, weight=1)
+
+        # Sidebar
+        self.sidebar_frame = SidebarFrame(self.body_frame, fg_color="#222222")
+        self.sidebar_frame.grid_columnconfigure(0, weight=1)
+        self.sidebar_frame.grid(
+            row=0,
+            column=0,
+            sticky="nsew",
+        )
+        
+        # Sidebar - Icon and Title
+        icon_and_title_frame = customtkinter.CTkFrame(
+            self.sidebar_frame, fg_color="transparent"
+        )
+        lock_image = customtkinter.CTkImage(
+            light_image=Image.open("./assets/app_icon.png"),
+            dark_image=Image.open("./assets/app_icon.png"),
+            size=(48, 48),
+        )
+        image_label = customtkinter.CTkLabel(
+            icon_and_title_frame, image=lock_image, text=""
+        )
+        title = customtkinter.CTkLabel(
+            icon_and_title_frame,
+            text="Locksmith\nPassword Manager",
+            justify="left",
+            font=customtkinter.CTkFont(family="Inter", size=18, weight="bold"),
+        )
+        icon_and_title_frame.grid(row=0, column=0)
+        image_label.grid(row=0, column=0, pady=8)
+        title.grid(row=0, column=1)
 
 
 app = App()
