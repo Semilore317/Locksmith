9baec2074ad44e105dfa2a1d718937e61ff8aad1
Hermes <hermeschigoziri@gmail.com>
2025-01-19 19:21:08 +0100

    enhance search bar dimensions in header frame

--- components/header_frame.py
+++ components/header_frame.py
@@ -6,6 +6,10 @@ class HeaderFrame(customtkinter.CTkFrame):
 
         self.grid_columnconfigure(0, weight=1)
         search_bar = customtkinter.CTkEntry(
-            master=self, placeholder_text="Search by Username or Website", width=300
+            master=self,
+            placeholder_text="Search by Username or Website",
+            width=600,
+            height=30,
+            corner_radius=3,
         )
         search_bar.grid(row=0, column=0, padx=10, pady=10)
