365bcc983f00b0079cbed357e81ed1d936208a5d
Hermes <hermeschigoziri@gmail.com>
2025-01-29 20:00:31 +0100

    feat: add AllItemsFrame to display all items in the application

---
+++ components/frames/all_items_frame.py
@@ -0,0 +1,34 @@
+import customtkinter as ctk
+
+
+# This frame shows all items (passwords, secure notes, etc.)
+class AllItemsFrame(ctk.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+
+        title_label = ctk.CTkLabel(
+            self,
+            text="All Items",
+            font=ctk.CTkFont(family="Arial", size=16, weight="bold"),
+        )
+        title_label.grid(row=0, column=0, sticky="ew")
+
+        # Retrieve items and display them sorted by timestamp - descending order
+        all_items = []
+        if len(all_items) > 0:
+            # If items are found, display them
+            pass
+        else:
+            # If no items have been created, display a message saying so
+            no_items_label = ctk.CTkLabel(
+                self,
+                text="NO ITEMS",
+                font=ctk.CTkFont(family="Arial", size=48, weight="bold"),
+            )
+            no_items_label.grid(row=1, column=0, sticky="ew", pady=(240, 0))
+            desc_label = ctk.CTkLabel(
+                self,
+                text="You haven't saved any note or login credential",
+                font=ctk.CTkFont(family="Arial", size=16),
+            )
+            desc_label.grid(row=2, column=0, sticky="ew", pady=(10, 0))
