ab17fced60592807b40abfba37ca95579a609abe
Hermes <hermeschigoziri@gmail.com>
2025-01-29 21:06:16 +0100

    feat: update ItemsFrame to support switching between AllItemsFrame and BinItemsFrame

--- components/frames/items_frame.py
+++ components/frames/items_frame.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
-from components.frames.all_items_frame import AllItemsFrame
+from components.frames.items_frames.all_items_frame import AllItemsFrame
+from components.frames.items_frames.bin_items_frame import BinItemsFrame
 
 
 class ItemsFrame(ctk.CTkFrame):
@@ -9,8 +10,17 @@ class ItemsFrame(ctk.CTkFrame):
         # Configurations
         self.grid_columnconfigure(0, weight=1)
 
-        self.all_items_frame = AllItemsFrame(
-            self,
-            fg_color="#2F2F2F",
-        )
-        self.all_items_frame.grid(row=0, column=0)
+        # Show all items by default
+        self.current_frame = AllItemsFrame(self, fg_color="transparent")
+        self.current_frame.grid(row=0, column=0)
+
+    def __switch_frame(self, frame: ctk.CTkFrame):
+        self.current_frame.destroy()
+        self.current_frame = frame(self, fg_color="transparent")
+        self.current_frame.grid(row=0, column=0)
+
+    def show_all_items(self):
+        self.__switch_frame(AllItemsFrame)
+
+    def show_bin_items(self):
+        self.__switch_frame(BinItemsFrame)
