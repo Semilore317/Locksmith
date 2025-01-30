a600143b7f03be1450bdb73b3be0a21cbb9a6c13
Hermes <hermeschigoziri@gmail.com>
2025-01-30 11:48:13 +0100

    refactor: rename AddLoginCredentialsFrame to AddItemsFrame

--- components/frames/cru_frame.py
+++ components/frames/cru_frame.py
@@ -1,7 +1,7 @@
 # CUR - Create, Update, Read
 import customtkinter as ctk
 
-from components.frames.cur_frames.add_login_frame import AddLoginCredentialsFrame
+from components.frames.cur_frames.add_items_frame import AddItemsFrame
 from components.frames.cur_frames.add_note_frame import AddNotesFrame
 
 
@@ -13,17 +13,14 @@ class CRUFrame(ctk.CTkFrame):
         self.grid_columnconfigure(0, weight=1)
         self.configure(fg_color="#464646")
 
-        # Show add login credentials frame by default
-        self.current_frame = AddLoginCredentialsFrame(self, fg_color="transparent")
-        self.current_frame.grid(row=0, column=0)
+        # Show add items frame by default
+        self.current_frame = AddItemsFrame(self, fg_color="transparent")
+        self.current_frame.grid(row=0, column=0, sticky="ew")
 
     def __switch_frame(self, frame: ctk.CTkFrame):
         self.current_frame.destroy()
         self.current_frame = frame(self, fg_color="transparent")
         self.current_frame.grid(row=0, column=0)
 
-    def show_add_login_frame(self):
-        self.__switch_frame(AddLoginCredentialsFrame)
-
-    def show_add_notes_frame(self):
-        self.__switch_frame(AddNotesFrame)
+    def show_add_items_frame(self):
+        self.__switch_frame(AddItemsFrame)
