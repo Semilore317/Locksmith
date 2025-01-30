7150e9820ecbd895450965563c61ffda389fed51
Hermes <hermeschigoziri@gmail.com>
2025-01-30 10:09:57 +0100

    feat: implement frame switching for adding login credentials and notes

--- components/frames/cru_frame.py
+++ components/frames/cru_frame.py
@@ -1,6 +1,9 @@
 # CUR - Create, Update, Read
 import customtkinter as ctk
 
+from components.frames.cur_frames.add_login_frame import AddLoginCredentialsFrame
+from components.frames.cur_frames.add_note_frame import AddNotesFrame
+
 
 class CRUFrame(ctk.CTkFrame):
     def __init__(self, master, **kwargs):
@@ -9,3 +12,18 @@ class CRUFrame(ctk.CTkFrame):
         # Configurations
         self.grid_columnconfigure(0, weight=1)
         self.configure(fg_color="#464646")
+
+        # Show add login credentials frame by default
+        self.current_frame = AddLoginCredentialsFrame(self, fg_color="transparent")
+        self.current_frame.grid(row=0, column=0)
+
+    def __switch_frame(self, frame: ctk.CTkFrame):
+        self.current_frame.destroy()
+        self.current_frame = frame(self, fg_color="transparent")
+        self.current_frame.grid(row=0, column=0)
+
+    def show_add_login_frame(self):
+        self.__switch_frame(AddLoginCredentialsFrame)
+
+    def show_add_notes_frame(self):
+        self.__switch_frame(AddNotesFrame)
