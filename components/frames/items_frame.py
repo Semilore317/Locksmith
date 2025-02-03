5e223e4fcae5a5396b857f4efb5d915532055822
Hermes <hermeschigoziri@gmail.com>
2025-02-03 13:38:44 +0100

    put the app back in a working state

--- components/frames/items_frame.py
+++ components/frames/items_frame.py
@@ -1,8 +1,10 @@
 import customtkinter as ctk
+from backend.models import LoginItemModel, NoteItemModel
 from components.frames.items_frames.all_items_frame import AllItemsFrame
 from components.frames.items_frames.bin_items_frame import BinItemsFrame
 from components.frames.items_frames.logins_frame import LoginItemsFrame
 from components.frames.items_frames.notes_frame import NoteItemsFrame
+from components.frames.items_frames.search_results_frame import SearchResultsFrame
 
 
 class ItemsFrame(ctk.CTkScrollableFrame):
@@ -25,13 +27,17 @@ class ItemsFrame(ctk.CTkScrollableFrame):
         self.canvas = self._parent_canvas
 
         # Enable mouse wheel scrolling
-        self.bind("<Enter>", self._bind_scroll_events)  # Activate scrolling when mouse enters
-        self.bind("<Leave>", self._unbind_scroll_events)  # Deactivate scrolling when mouse leaves
+        self.bind(
+            "<Enter>", self._bind_scroll_events
+        )  # Activate scrolling when mouse enters
+        self.bind(
+            "<Leave>", self._unbind_scroll_events
+        )  # Deactivate scrolling when mouse leaves
 
-    def __switch_frame(self, frame: ctk.CTkFrame):
+    def __switch_frame(self, frame: ctk.CTkFrame, **kwargs):
         self.current_frame.destroy()
         self.current_frame = frame(
-            self, fg_color="transparent", controllers=self.controllers
+            self, fg_color="transparent", controllers=self.controllers, **kwargs
         )
         self.current_frame.grid(row=0, column=0, sticky="nsew", padx=6, pady=6)
 
@@ -48,11 +54,18 @@ class ItemsFrame(ctk.CTkScrollableFrame):
     def show_secure_notes(self):
         self.__switch_frame(NoteItemsFrame)
 
+    def show_search_results(self, results: list[LoginItemModel | NoteItemModel]):
+        self.__switch_frame(SearchResultsFrame, search_results=results)
+
     def _bind_scroll_events(self, event=None):
         """Bind scrolling events when the mouse is over the frame."""
         self.canvas.bind_all("<MouseWheel>", self._on_scroll)  # Windows/macOS
-        self.canvas.bind_all("<Button-4>", lambda e: self.canvas.yview_scroll(-1, "units"))  # Linux Scroll Up
-        self.canvas.bind_all("<Button-5>", lambda e: self.canvas.yview_scroll(1, "units"))   # Linux Scroll Down
+        self.canvas.bind_all(
+            "<Button-4>", lambda e: self.canvas.yview_scroll(-1, "units")
+        )  # Linux Scroll Up
+        self.canvas.bind_all(
+            "<Button-5>", lambda e: self.canvas.yview_scroll(1, "units")
+        )  # Linux Scroll Down
 
     def _unbind_scroll_events(self, event=None):
         """Unbind scrolling events when the mouse leaves the frame."""
